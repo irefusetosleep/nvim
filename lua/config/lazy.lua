646f4dfa2df31be3933adc624e7fb5a70b027e34
irefusetosleep <lewisj13122005@gmail.com>
2024-08-18 16:22:44 +0100

    it wasnt even treesitters fault mb

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,10 +24,10 @@ vim.g.maplocalleader = ","
 vim.opt.number = true          -- Shows absolute line numbers
 vim.opt.relativenumber = false  -- Shows relative line numbers
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 -- Setup lazy.nvim
 require("lazy").setup({
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,35 +1,38 @@
 return {
-    {
-      "nvim-telescope/telescope.nvim",
-      keys = {
-        -- disable the keymap to grep files
-        {"<leader>/", false},
-        -- change a keymap
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep"},
-        -- add a keymap to browse plugin files
-        {
-          "<leader>fp",
-          function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-          desc = "Find Plugin File",
-        },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- disable the keymap to grep files
+      {"<leader>/", false},
+      -- change a keymap
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep"},
+      -- add a keymap to browse plugin files
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
       },
-      dependancies = {"nvim-treesitter/nvim-treesitter"}
+    },
+    dependancies = {"nvim-treesitter/nvim-treesitter"},
+    preview = {
+      treesitter = false, -- Disable Tree-sitter in previews
+    },
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-        require("telescope").setup {
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {
-              }
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
             }
           }
         }
-        -- To get ui-select loaded and working with telescope, you need to call
-        -- load_extension, somewhere after setup function:
-        require("telescope").load_extension("ui-select")
+      }
+      -- To get ui-select loaded and working with telescope, you need to call
+      -- load_extension, somewhere after setup function:
+      require("telescope").load_extension("ui-select")
     end
   }
 }
